7369ccf7e7f3b88420c70f4783b9d797cfcc3902
sjaak.meulen@philips.com <sjaak.meulen@philips.com>
2020-01-30 15:01:34 +0100

    Face detection added

---
+++ public/workshop.js
@@ -0,0 +1,69 @@
+const displaySize = { width: 640, height: 480 }
+const init = async () => {
+  const MODEL_URL = '/models';
+
+  await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
+  await faceapi.loadFaceLandmarkModel(MODEL_URL)
+  await faceapi.loadFaceRecognitionModel(MODEL_URL)
+
+  const canvas = document.getElementById('overlay')
+  faceapi.matchDimensions(canvas, displaySize)
+
+  const videoEl = document.getElementById('inputVideo')
+  navigator.getUserMedia(
+    { video: {} },
+    stream => videoEl.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+async function onPlay(videoEl) {
+  const input = document.getElementById('inputVideo')
+  const canvas = document.getElementById('overlay')
+
+  let fullFaceDescriptions = await faceapi.detectAllFaces(input).withFaceLandmarks().withFaceDescriptors()
+  if (fullFaceDescriptions.length > 0) {
+    await detectPersons(fullFaceDescriptions, canvas);
+    fullFaceDescriptions = faceapi.resizeResults(fullFaceDescriptions, displaySize);
+  }
+
+  setTimeout(() => onPlay(videoEl))
+}
+
+async function detectPersons(fullFaceDescriptions, canvas) {
+  const context = canvas.getContext('2d');
+  const labels = ['sjaak', 'richard', 'arvid', 'mara', 'frank', 'wilma', 'marcin', 'deha', 'slave']
+
+  const labeledFaceDescriptors = await Promise.all(
+    labels.map(async label => {
+      // fetch image data from urls and convert blob to HTMLImage element
+      const imgUrl = `labels/${label}.png`
+      const img = await faceapi.fetchImage(imgUrl)
+
+      // detect the face with the highest score in the image and compute it's landmarks and face descriptor
+      const fullFaceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+
+      if (!fullFaceDescription) {
+        throw new Error(`no faces detected for ${label}`)
+      }
+
+      const faceDescriptors = [fullFaceDescription.descriptor]
+      return new faceapi.LabeledFaceDescriptors(label, faceDescriptors)
+    })
+  )
+
+  const maxDescriptorDistance = 0.6
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance)
+  const results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor))
+  console.log(results[0]._label, results[0]._distance, results);
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  results.forEach((bestMatch, i) => {
+    const box = fullFaceDescriptions[i].detection.box
+    const text = bestMatch.toString()
+    const drawBox = new faceapi.draw.DrawBox(box, { label: text })
+    drawBox.draw(canvas)
+  })
+}
+
+init().then(() => console.log("Done"));
