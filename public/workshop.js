8d282b64fa2160b7ce188e1a0257c189e2349a35
sjaak.meulen@philips.com <sjaak.meulen@philips.com>
2020-01-30 15:33:44 +0100

    Performance improvement

--- public/workshop.js
+++ public/workshop.js
@@ -1,4 +1,6 @@
 const displaySize = { width: 640, height: 480 }
+let labeledFaceDescriptors = [];
+
 const init = async () => {
   const MODEL_URL = '/models';
 
@@ -7,6 +9,26 @@ const init = async () => {
   await faceapi.loadFaceRecognitionModel(MODEL_URL)
 
   const canvas = document.getElementById('overlay')
+  const labels = ['sjaak', 'richard', 'arvid', 'mara', 'frank', 'wilma', 'marcin', 'deha', 'slave', 'louis', 'jeroen']
+
+  labeledFaceDescriptors = await Promise.all(
+    labels.map(async label => {
+      // fetch image data from urls and convert blob to HTMLImage element
+      const imgUrl = `labels/${label}.png`
+      const img = await faceapi.fetchImage(imgUrl)
+
+      // detect the face with the highest score in the image and compute it's landmarks and face descriptor
+      const fullFaceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+
+      if (!fullFaceDescription) {
+        throw new Error(`no faces detected for ${label}`)
+      }
+
+      const faceDescriptors = [fullFaceDescription.descriptor]
+      return new faceapi.LabeledFaceDescriptors(label, faceDescriptors)
+    })
+  );
+  
   faceapi.matchDimensions(canvas, displaySize)
 
   const videoEl = document.getElementById('inputVideo')
@@ -32,30 +54,9 @@ async function onPlay(videoEl) {
 
 async function detectPersons(fullFaceDescriptions, canvas) {
   const context = canvas.getContext('2d');
-  const labels = ['sjaak', 'richard', 'arvid', 'mara', 'frank', 'wilma', 'marcin', 'deha', 'slave']
-
-  const labeledFaceDescriptors = await Promise.all(
-    labels.map(async label => {
-      // fetch image data from urls and convert blob to HTMLImage element
-      const imgUrl = `labels/${label}.png`
-      const img = await faceapi.fetchImage(imgUrl)
-
-      // detect the face with the highest score in the image and compute it's landmarks and face descriptor
-      const fullFaceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-
-      if (!fullFaceDescription) {
-        throw new Error(`no faces detected for ${label}`)
-      }
-
-      const faceDescriptors = [fullFaceDescription.descriptor]
-      return new faceapi.LabeledFaceDescriptors(label, faceDescriptors)
-    })
-  )
-
   const maxDescriptorDistance = 0.6
   const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance)
   const results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor))
-  console.log(results[0]._label, results[0]._distance, results);
 
   context.clearRect(0, 0, canvas.width, canvas.height);
   results.forEach((bestMatch, i) => {
